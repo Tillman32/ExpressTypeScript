ec66f7f975d41a10688a8afa91d2bf851fd64e0d
Tillman32 <brandon@brandontillman.com>
2016-09-04 02:42:40 -0700

    Added envrionment variables

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,8 @@
 "use strict";
 var express = require('express');
+var dotenv = require('dotenv');
 var service_1 = require('./services/service');
-var service = new service_1.Service({ app: express(), port: 3000 });
+dotenv.load({ path: '.env.example' });
+var service = new service_1.Service({ app: express(), port: process.env.SERVICE_PORT });
 service.Run();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 /// <reference path="../typings/index.d.ts" />
 import express = require('express');
+import dotenv = require('dotenv');
 import {Service} from './services/service';
 
-var service = new Service({app: express(), port: 3000});
+dotenv.load({path: '.env.example'});
+
+var service = new Service({app: express(), port: process.env.SERVICE_PORT});
 service.Run();
