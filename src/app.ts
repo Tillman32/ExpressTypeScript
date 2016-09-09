325c3a2218488a447cc4bd1cc7b69bad24b53db8
Tillman32 <brandon@brandontillman.com>
2016-09-08 21:32:02 -0700

    Added a config.ts file to house upcoming configs for passport.js and db connection strings.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/index.d.ts" />
 import express = require('express');
+import {Config} from './config'
 import {Service} from './services/service';
 
-var service = new Service({app: express(), port: process.env.SERVICE_PORT});
+var service = new Service({app: express(), port: Config.Port});
 service.Run();
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+/*
+    Configuration
+*/
+
+export class Config {
+    // Express service port
+    static Port: number = 3000;
+}
